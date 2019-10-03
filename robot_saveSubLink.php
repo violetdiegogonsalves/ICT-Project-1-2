5400deb76c5ffa930aeb31d0c89f80b907dcd4da
VIOLET GONSALVES <39324614+violetdiegogonsalves@users.noreply.github.com>
2019-10-03 12:40:48 +1000

    Add files via upload

---
+++ robot_saveSubLink.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once("config.php");
+
+$imageData = isset($_POST["sub_id"]) ? $_POST["sub_id"] : false;
+
+if ($imageData) {
+
+
+    $output = '';
+    $query = "update robot_assets_sub set link='".$_POST["link1"]."' where id = '".$_POST["sub_id"]."'";
+    $connect = db_connect();
+    $result = mysqli_query($connect, $query);
+
+    if (!$result = $connect->query($query)) {
+        $err
+            = "QUERY FAIL: "
+            . $query
+            . ' ERRNO: '
+            . $conn->errno
+            . ' ERROR: '
+            . $conn->error
+        ;
+        trigger_error($err, E_USER_ERROR);
+    }
+
+
+    $output .= 'updated success';
+    echo $output;
+}
+
+
+
+
+?>
---
+++ robot_uploadSub.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once("config.php");
+$imageData = isset($_POST["imageData1"]) ? $_POST["imageData1"] : false;
+
+if ($imageData) {
+
+    $getQuery = "SELECT image FROM robot_assets_sub WHERE id = '".$_POST["sub_id"]."'";
+    $connect = db_connect();
+    $resultSet = mysqli_query($connect, $getQuery);
+    while ($row = $resultSet->fetch_object()) {
+        $imageURL = $row->image;
+        if($imageURL != null && $imageURL !=''){
+            unlink($imageURL);
+        }
+    }
+
+    define('UPLOAD_DIR', '../uploads/images/asset/sub/');
+    $image_parts = explode(";base64,", $_POST["imageData1"]);
+    $image_type_aux = explode("image/", $image_parts[0]);
+    $image_type = $image_type_aux[1];
+    $image_base64 = base64_decode($image_parts[1]);
+    $file = UPLOAD_DIR . uniqid() . '.' . $image_type;
+    file_put_contents($file, $image_base64);
+
+    $output = '';
+    $query = "update robot_assets_sub set image='".$file."' where id = '".$_POST["sub_id"]."'";
+    $result = mysqli_query($connect, $query);
+
+    if (!$result = $connect->query($query)) {
+        $err
+            = "QUERY FAIL: "
+            . $query
+            . ' ERRNO: '
+            . $conn->errno
+            . ' ERROR: '
+            . $conn->error
+        ;
+        trigger_error($err, E_USER_ERROR);
+    }
+
+
+    $output .= 'upload success';
+    echo $output;
+}
+
+?>
